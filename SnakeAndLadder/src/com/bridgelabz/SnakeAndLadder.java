851f42aae5f2cab72f0aefde00600018135a6ac9
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-07 14:37:28 +0530

    Added

--- SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
+++ SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
@@ -1,29 +1,16 @@
 package com.bridgelabz;
 
-import java.util.Random;
-
 public class SnakeAndLadder {
-	public static final int NO_PLAY = 1;
-	public static final int LADDER = 2;
-	public static final int SNAKE =3;
+	private static int RandomDieValue() {
+		int RandomValue = (int) Math.floor((Math.random() * 6) + 1);
+		return RandomValue;
+	}
+	
 	public static void main(String[] args) {
-		int position = 0;
-		System.out.println("First player is at postion "+position);
-		
-		Random random = new Random();
-		int dieNum = random.nextInt(7-1)+1;
-		System.out.println("Dice number : "+dieNum);
-		
-		int choice = random.nextInt(4-1)+1;
-		
-		if(choice == NO_PLAY){
-			System.out.println("No Play");
-		}else if(choice == LADDER) {
-			System.out.println("Player inclined by "+dieNum);
-			position+=dieNum;
-		}else if(choice == SNAKE) {
-			System.out.println("Player declined by "+dieNum);
-			position-=dieNum;
-	    }
+		int startingPosition = 0;
+		int numberOnDie;
+		System.out.println("Initial position of player is : " +startingPosition);
+		numberOnDie = RandomDieValue();
+		System.out.println("Number on Die is : " +numberOnDie);	
     }
 }

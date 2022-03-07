8dd20b8e035ab6d70b0b916b5bd2f4e434436560
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-07 14:50:20 +0530

    Adding

--- SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
+++ SnakeAndLadder/src/com/bridgelabz/SnakeAndLadder.java
@@ -1,16 +1,41 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
-	private static int RandomDieValue() {
-		int RandomValue = (int) Math.floor((Math.random() * 6) + 1);
-		return RandomValue;
-	}
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 	
 	public static void main(String[] args) {
-		int startingPosition = 0;
-		int numberOnDie;
-		System.out.println("Initial position of player is : " +startingPosition);
-		numberOnDie = RandomDieValue();
-		System.out.println("Number on Die is : " +numberOnDie);	
+		int playerCurrentPosition = 0;
+		System.out.println("Initial position of player is : " +playerCurrentPosition);
+		int numberOnDie = RandomDieValue();
+		System.out.println("Number on Die is : " +numberOnDie);
+		int option = RandomOption();
+		System.out.println("The option is " +option);
+		
+		switch(option) {
+		case LADDER :
+			playerCurrentPosition+=numberOnDie;
+			break;
+		case SNAKE:
+			playerCurrentPosition-=numberOnDie;
+			if (playerCurrentPosition < 0)
+				playerCurrentPosition = 0;
+			break;
+		default:
+			System.out.println("No play");
+		}
+		System.out.println(playerCurrentPosition);		
+	}
+
+	private static int RandomOption() {
+		int RandomOption = (int) Math.floor((Math.random() * 3));
+		return RandomOption;
+	}
+
+	private static int RandomDieValue() {
+		int RandomDieValue = (int) Math.floor((Math.random() * 6) + 1);
+		return RandomDieValue;
+	
     }
 }
